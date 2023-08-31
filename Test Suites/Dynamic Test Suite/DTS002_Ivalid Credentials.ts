d8ea1ab333c8489102b356d8a9b9649f2dbbd6be
HannachiHassen <hannachihassen07@gmail.com>
2023-08-31 12:45:31 -0400

    Defining and initializing Groovy variables in Katalon Studio

---
+++ Test Suites/Dynamic Test Suite/DTS002_Ivalid Credentials.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>DTS002_Ivalid Credentials</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5135d960-8647-419c-abea-260562120a11</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>tag=(regression) </filteringText>
+</FilteringTestSuiteEntity>
