cf2f80ac2811c943b2b5fa7b97aa124f0bec3cff
HannachiHassen <hannachihassen07@gmail.com>
2023-09-05 14:32:56 -0400

    Running Tests with TestClouds in katalon Studio

---
+++ Test Suites/Test Suite/Suite 06 - Ecommerce.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite 06 - Ecommerce</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c402483d-5f11-46b1-abcc-0bbd947e7ceb</testSuiteGuid>
+   <testCaseLink>
+      <guid>61a27e24-23cf-45d6-b7b6-b75e39a2f290</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reuse Test Case/E-commerce/TC001_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ea36b00d-7ead-44ee-84bc-bffd5ae81ff3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reuse Test Case/E-commerce/TC002_Add to cart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e4a584ea-4a38-43b9-bdf1-9ca048c22aa9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reuse Test Case/E-commerce/TC003_Checkout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
