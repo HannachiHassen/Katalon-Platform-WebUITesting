edb30b5a49c552c4b3c14cfe6b707d5a7267e820
HannachiHassen <hannachihassen07@gmail.com>
2023-09-07 12:46:39 -0400

    Simple Steps to Create Visual Tests in Katalon

---
+++ Test Suites/Visual Regression/Visual Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0cd5f4b4-8478-4829-a38f-d625b21abe56</testSuiteGuid>
+</TestSuiteEntity>
